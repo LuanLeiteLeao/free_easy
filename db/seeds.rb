23d8cacba8a9339562dce1f1c5d42e7f117af762
luanleiteleao <luanleiteraio@gmail.com>
2021-10-12 17:10:46 -0300

    listar projetos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-project = Project.create({
+Project.create({
     title:'ERP para áre de aviação missionária',
     description:'Asas de Socorro é uma organização cristã missionária. Entidade sem fins lucrativos- de caráter beneficente, filantrópico, cultural, educativo, religioso, de assistência social e de direitos humanos.Asas de Socorro fornece apoio logístico para programas assistências desenvolvidos em áreas remotas, e ainda desenvolve programas culturais, educacionais e de saúde- especialmente nas regiões mais carentes de transporte e comunicação da Amazônia.\
     precisamos de um sistema que contemple todas as nossas necessidades em um só sistema, que seja voltado para a nossas regras de negócio que aviação e ações missionárias',
--- spec/system/professional_see_projects_spec.rb
+++ spec/system/professional_see_projects_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Professional visit home page' do
   it 'and view projects' do
     #arrange
-    project = Project.create({
+    Project.create!({
         title:'ERP Para Escola',
         description:'precisamos de um sistema que contemple todas as nossas necessidades em um só sistema',
         skills_descritions:'Pyton, React JS, React Native, AWS, Docker',
@@ -20,8 +20,8 @@ describe 'Professional visit home page' do
     expect(page).to have_content('ERP Para Escola')
     expect(page).to have_content('precisamos de um sistema que contemple todas as nossas necessidades em um só sistema')
     expect(page).to have_content('Pyton, React JS, React Native, AWS, Docker')
-    expect(page).to have_content('100.0')
-    expect(page).to have_content('0')
+    expect(page).to have_content('R$ 100,00/Hrs')
+    expect(page).to have_content('face_to_face')
 
   end
 end
